cbc6360e5659962d5f1ab8c105001524a80dad3c
V.Zakharov <stin-plus@yandex.ru>
2024-06-27 17:55:06 +0400

    Add new response classes

---
+++ YandexClasses/CampaignResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexClasses
+{
+    public class CampaignResponse
+    {
+        public List<CampaignDTO> Campaigns { get; set; }
+    }
+
+    public class CampaignDTO
+    {
+        public string Domain { get; set; }
+        public string Id { get; set; }
+        public string ClientId { get; set; }
+        public BusinessDTO Business { get; set; }
+        public string PlacementType { get; set; }
+    }
+
+    public class BusinessDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
